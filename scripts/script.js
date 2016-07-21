ec1a7c44b4a864e1c6ecdfce5a37f40d4f3518e8
Simpleprune <Simpleprune@gmail.com>
2016-07-21 14:00:41 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,7 @@
 var main = function(){
-  $('.bougie').click(function(){
-    $(this).fadeOut('slow');
-    });
-  $("#gateau").click(function(){
+   $("#gateau").click(function(){
     $(this).fadeOut('slow');
   });
-  });
+  };
   
   $(document).ready(main);
