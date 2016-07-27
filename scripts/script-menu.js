8d6bf1b4d367eebfd6083e8162a2ca684848ec17
Simpleprune <Simpleprune@gmail.com>
2016-07-27 16:22:14 +0200

    Update script-menu.js

--- scripts/script-menu.js
+++ scripts/script-menu.js
@@ -2,7 +2,7 @@ var main = function(){
   $('.card').hover(function(){
     $(this).addClass('card-preview');
   },
-  function(){$(this).addClass('card-preview');}
+  function(){$(this).removeClass('card-preview');}
   );
   $('.card').click(function(){
     $(this).toggleClass('selected');
