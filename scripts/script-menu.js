356b87c8c96ebbf0fdbbc20c9c74f0aa829783e9
Simpleprune <Simpleprune@gmail.com>
2016-07-27 16:09:16 +0200

    Update script-menu.js

--- scripts/script-menu.js
+++ scripts/script-menu.js
@@ -4,6 +4,10 @@ var main = function(){
   },
   function(){$(this).addClass('card-preview');}
   );
+  $('.card').click(function(){
+    $(this).toggleClass('selected');
+    $(this).('preview').fadeOut();
+  });
 };
 
 $(document).ready(main);
